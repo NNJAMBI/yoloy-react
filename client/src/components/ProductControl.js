6d495b66b92e5f136e28e084e77cd61117acd930
Nancy Njambi Karanja <nnjambi@safaricom.co.ke>
2020-09-02 22:43:03 +0300

    add all changes

--- client/src/components/ProductControl.js
+++ client/src/components/ProductControl.js
@@ -5,66 +5,24 @@ import ProductList from './ProductList';
 import NewProductForm from './NewProductForm';
 import ProductDetail from './ProductDetails';
 
-import tshirt from '../images/products/tshirt.png';
-import backpack from '../images/products/backpack.png';
-import pants from '../images/products/pants.png';
-import trekkingshoes from '../images/products/trekkingshoes.png';
-import giacket from '../images/products/giacket.png';
-import tshirt_ladies from '../images/products/tshirt_ladies.png';
-import Default_image from '../images/product_image.jpeg/product_image.jpeg';
-
-// ActualProductList
-const actualProductList = [
-    
-   
-    {
-        name: 'T-Shirt',
-        price: '599',
-        photo: tshirt,
-        id: "1"
-    },
-    {
-        name: 'BackPack', 
-        price: '1500',
-        photo: backpack,
-        id: "2"
-    },
-    {
-        name: 'Pants', 
-        price: '1000',
-        photo: pants,
-        id: '3'
-    },
-    {
-        name: 'Trekking Shoes',
-        price: '2000',
-        photo: trekkingshoes,
-        id: '4'
-    },
-    {
-        name: 'Jacket',
-        price: '1500',
-        photo: giacket,
-        id: '5'
-    },
-    {
-        name:'T-Shirt Ladies',
-        price: '650',
-        photo: tshirt_ladies,
-        id: '6'
-    }
-]
-
-
 
 class ProductControl extends Component {
     constructor(props){
         super(props);
         this.state ={
             productFormVisible: false,
-            actualProductList: actualProductList,
+            actualProductList: [],
             selectedProduct: null
         }
+    }
+    componentDidMount(){
+        axios.get('http://localhost:5000/products')
+        .then(res =>{
+            console.log(res)
+            this.setState({
+                actualProductList: res.data
+            })
+        })
     }
 
     handleClick = ()=>{
@@ -91,6 +49,18 @@ class ProductControl extends Component {
         productFormVisible: false
     })
 };
+handleDeletingProduct = (id) =>{
+    axios.delete('http://localhost:5000/products/'+id)
+        .then(res => console.log(res.data))
+        .catch((error) =>{
+            console.log(error)
+        })
+        this.setState({
+            actualProductList: this.state.actualProductList.filter(product => product._id !== id),
+            formVisibleOnPage: false,
+            selectedProduct: null
+        })
+}
     // Method to handle click event on a product
     handleChangingSelectedProduct = (id) => {
         const selectedProduct = this.state.actualProductList.filter(product => product.id === id)[0];
@@ -99,9 +69,10 @@ class ProductControl extends Component {
     render() {
         let currentVisibleState = null;
         let buttonText = null
-        if(this.state.selectedProduct != null){
-            currentVisibleState = <ProductDetail  product ={this.state.selectedProduct} /> //new code
-            buttonText = 'Back to Product List '
+        if (this.state.selectedProduct != null) {
+            currentVisibleState = <ProductDetail product = {this.state.selectedProduct} onDeleteProduct = {this.handleDeletingProduct}
+         />
+            buttonText = "Back to product list";
         } else if(this.state.productFormVisible){
             currentVisibleState = <NewProductForm  onNewProductCreation= {this.handleAddingNewProduct}/>
             buttonText = 'Go back to Product List'
